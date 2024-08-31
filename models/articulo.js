6ea374c6b2ced895d26fef98ea45c88469415b2d
William <49001552+Andres715@users.noreply.github.com>
2024-08-30 19:29:05 -0600

    Add files via upload

---
+++ models/articulo.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+const articuloSchema = new mongoose.Schema({}, { strict: false });
+
+const Articulo = mongoose.model("Articulo", articuloSchema);
+
+module.exports = Articulo;
